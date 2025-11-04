df99e50f59f57677711ff42f2b3a1ff1cc2eceb0
annal <annal@LAPTOP-VPDOBI63>
2025-11-04 10:46:05 +0530

    vikicommit

---
+++ vikas_selenum/src/test/java/Automation_pac/implicit_wait.java
@@ -0,0 +1,24 @@
+package Automation_pac;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class implicit_wait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.instagram.com/");
+		driver.findElement(By.xpath("//span[@class='x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x1iiplp5 x10wh9bi xpm28yp x8viiok x1o7cslx']")).click();
+		driver.quit();
+		
+	}
+
+}
